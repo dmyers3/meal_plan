48377b819507330a475caa01a2b34ebbb6455b7a
Daniel Myers <daniel.p.myers@gmail.com>
2017-07-28 12:30:20 +0000

    changed sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDISTOGO_URL"] || "redis://localhost:6379/", size: 5 }
+  config.redis = { url: ENV["REDISTOGO_URL"] || "redis://localhost:6379/", size: 7 }
 end
 
 Sidekiq.configure_client do |config|
