bc53412a7c0bae6f3c1fc1e9322f0bf21e7fe6ee
Daniel Myers <daniel.p.myers@gmail.com>
2017-07-28 01:30:43 +0000

    updated mailer to use id instead of user object when passed

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -2,7 +2,8 @@ class AppMailer < ActionMailer::Base
   layout 'mailer'
   add_template_helper(ApplicationHelper)
   
-  def welcome(user)
+  def welcome(user_id)
+    user = User.find(user_id)
     mail from: "Pass The Cheddar <support@passthecheddar.com>", to: user.email, subject: "Welcome to Pass The Cheddar!"
   end
 end
