5d9169d95615f1ef939ad8a56826fa4563f87be2
Daniel Myers <daniel.p.myers@gmail.com>
2017-07-28 20:38:14 +0000

    updated staging and production host urls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   
-  config.action_mailer.default_url_options = { :host => 'dmyers3-meal-plan.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://dmyers3-meal-plan.herokuapp.com' }
   
   config.action_mailer.smtp_settings = {
     :address => 'smtp-relay.sendinblue.com',
