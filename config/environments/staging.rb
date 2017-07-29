a70f28008b64adb14f88a538c27fdf2053319821
Daniel Myers <daniel.p.myers@gmail.com>
2017-07-29 15:22:01 +0000

    add initializer for action_mailer

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -69,17 +69,17 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
   
-  config.action_mailer.default_url_options = { :host => 'https://dmyers3-meal-plan-staging.herokuapp.com' }
+  # config.action_mailer.default_url_options = { :host => 'https://dmyers3-meal-plan-staging.herokuapp.com' }
   
-  config.action_mailer.smtp_settings = {
-    :address => 'smtp-relay.sendinblue.com',
-    :port => 587,
-    :domain => 'dmyers3-meal-plan-staging.herokuapp.com',
-    :user_name => 'daniel.p.myers@gmail.com',
-    :password => ENV['SENDINBLUE_PASSWORD'],
-    :authentication => 'login',
-    :enable_starttls_auto => true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   :address => 'smtp-relay.sendinblue.com',
+  #   :port => 587,
+  #   :domain => 'dmyers3-meal-plan-staging.herokuapp.com',
+  #   :user_name => 'daniel.p.myers@gmail.com',
+  #   :password => ENV['SENDINBLUE_PASSWORD'],
+  #   :authentication => 'login',
+  #   :enable_starttls_auto => true
+  # }
 end
---
+++ config/initializers/action_mailer.rb
@@ -0,0 +1,52 @@
+if Rails.env.development?
+
+  # Settings for mailcatcher on dev enviroment
+  Rails.application.config.action_mailer.tap do |action_mailer|
+   action_mailer.default_url_options = { :host => "https://meal-plan-dmyers3.c9users.io" }
+
+    action_mailer.delivery_method = :letter_opener
+    action_mailer.perform_deliveries = true
+    action_mailer.raise_delivery_errors = false
+    action_mailer.perform_caching = false
+  end
+end
+
+if Rails.env.production?
+  # Define settings for Production SMTP Server
+  Rails.application.config.action_mailer.tap do |action_mailer|
+    action_mailer.default_url_options = { :host => 'https://dmyers3-meal-plan.herokuapp.com' }
+
+    action_mailer.delivery_method = :smtp
+    action_mailer.perform_deliveries = true
+    action_mailer.raise_delivery_errors = true
+    action_mailer.smtp_settings = {
+    :address => 'smtp-relay.sendinblue.com',
+    :port => 587,
+    :domain => 'dmyers3-meal-plan.herokuapp.com',
+    :user_name => 'daniel.p.myers@gmail.com',
+    :password => ENV['SENDINBLUE_PASSWORD'],
+    :authentication => 'login',
+    :enable_starttls_auto => true
+  }
+  end
+end
+
+if Rails.env.staging?
+  # Define settings for Staging SMTP Server
+  Rails.application.config.action_mailer.tap do |action_mailer|
+    action_mailer.default_url_options = { :host => 'https://dmyers3-meal-plan-staging.herokuapp.com' }
+
+    action_mailer.delivery_method = :smtp
+    action_mailer.perform_deliveries = true
+    action_mailer.raise_delivery_errors = true
+    action_mailer.smtp_settings = {
+    :address => 'smtp-relay.sendinblue.com',
+    :port => 587,
+    :domain => 'dmyers3-meal-plan-staging.herokuapp.com',
+    :user_name => 'daniel.p.myers@gmail.com',
+    :password => ENV['SENDINBLUE_PASSWORD'],
+    :authentication => 'login',
+    :enable_starttls_auto => true
+  }
+  end
+end
