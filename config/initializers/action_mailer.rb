ea18dabd8cba112655c9a5f2bfc6588ea6bb6212
Daniel Myers <daniel.p.myers@gmail.com>
2017-07-30 01:33:05 +0000

    changed default mailer url to env variable

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -14,7 +14,7 @@ end
 if Rails.env.production?
   # Define settings for production SMTP Server
   Rails.application.config.action_mailer.tap do |action_mailer|
-    action_mailer.default_url_options = { :host => ENV[MAILER_URL] }
+    action_mailer.default_url_options = { :host => ENV['MAILER_URL'] }
 
     action_mailer.delivery_method = :smtp
     action_mailer.perform_deliveries = true
