a982bba08ad434cf4c49a53bf3fbee861661f610
Daniel Myers <daniel.p.myers@gmail.com>
2017-08-03 20:00:57 +0000

    add tags table

--- app/models/tag_category.rb
+++ app/models/tag_category.rb
@@ -1,4 +1,5 @@
 class TagCategory < ActiveRecord::Base
   has_many :tags
   validates_presence_of :name
+  validates_uniqueness_of :name
 end
--- spec/models/tag_category_spec.rb
+++ spec/models/tag_category_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe TagCategory do
   it { should validate_presence_of(:name) }
   it { should have_many(:tags) }
+  it { should validate_uniqueness_of(:name) }
 end
